f3b9029e24750b12942dd7c5ef80f0e2aacecf4f
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-09-13 14:37:32 +0800

    First commit

--- app/views/Showdata.php
+++ app/views/Showdata.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Showdata</title>
-    <link rel="stylesheet" href="<?=base_url();?>public/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
+
 
 
 </head>
@@ -33,7 +34,7 @@
     </td>
 </tr>
 <?php endforeach; ?>
-<a href="/user/create">Create Record</a>
+
         </table>
         <a href="/user/create">Create Record</a>
 
--- app/views/ViewProfile.php
+++ app/views/ViewProfile.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ViewProfile</title>
-<link rel="stylesheet" href="<?=base_url();?>public/css/style4.css">
+<link rel="stylesheet" href="/css/style4.css">
+
 </head>
 <body>
     <div class="panel">
