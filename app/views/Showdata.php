8354bbea0191e4645b21f1088426c9a484c054ca
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-09-13 13:12:58 +0800

    First commit

--- app/views/Showdata.php
+++ app/views/Showdata.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Showdata</title>
-    <link rel="stylesheet" href="<?=base_url();?>public/css/style.css">
+    <link rel="stylesheet" href="/public/css/style.css">
+
 </head>
 <body>
     <div class="center-panel">
