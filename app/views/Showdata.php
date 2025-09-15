8893884adcff96c6562d5910dc5d7d6e2ca14820
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-09-15 16:43:15 +0800

    update

--- app/views/Showdata.php
+++ app/views/Showdata.php
@@ -5,14 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Showdata</title>
     <link rel="stylesheet" href="/public/css/style.css">
-
-
-
 </head>
 <body>
     <div class="center-panel">
         <h1>SHOWDATA VIEW</h1>
-        <table border="1">
+
+        <table>
             <tr>
                 <th>ID</th>
                 <th>Last Name</th>
@@ -22,22 +20,21 @@
                 <th>Action</th>
             </tr>
             <?php foreach($students as $student): ?>
-<tr>
-    <td><?=html_escape($student['id']);?></td>
-    <td><?=html_escape($student['last_name']);?></td>
-    <td><?=html_escape($student['first_name']);?></td>
-    <td><?=html_escape($student['email']);?></td>
-    <td><?=html_escape($student['Role']);?></td>
-    <td>
-       <a href="/user/update/<?=$student['id'];?>">Update</a>
-       <a href="/user/soft-delete/<?=$student['id'];?>">Delete</a>
-    </td>
-</tr>
-<?php endforeach; ?>
-
+            <tr>
+                <td><?= html_escape($student['id']); ?></td>
+                <td><?= html_escape($student['last_name']); ?></td>
+                <td><?= html_escape($student['first_name']); ?></td>
+                <td><?= html_escape($student['email']); ?></td>
+                <td><?= html_escape($student['Role']); ?></td>
+                <td>
+                    <a href="/user/update/<?= $student['id']; ?>">Update</a>
+                    <a href="/user/confirm-delete/<?= $student['id']; ?>">Delete</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
         </table>
-        <a href="/user/create">Create Record</a>
 
+        <a href="/user/create" class="create-btn">Create Record</a>
     </div>
 </body>
 </html>
