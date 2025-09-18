86e34419686156ee2e30774f62848729f31ecf83
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-09-18 13:50:59 +0800

    updated

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798222',
-    'password'	=> 'GDT5VZxM4u',
-    'database'	=> 'sql12798222',
+    'username'	=> 'sql12799092',
+    'password'	=> 'arH7LNJYF8',
+    'database'	=> 'sql12799092',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
