c2c943b9ea503b99b23ee8280209c2bccfd8677d
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-09-18 08:49:44 +0800

    update

--- app/config/routes.php
+++ app/config/routes.php
@@ -45,7 +45,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-$router->get('/', 'UserController::show');
+$router->get('/', 'UserController::create');
 $router->get('/about', 'Welcome::about');
 
 $router->get('/user/profile/{username}/{name}', 'UserController::profile');
