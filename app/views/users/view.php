223844cb32cbe68fa8501d513d9758ba97d8de1d
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-09-24 22:43:19 +0800

    updated

--- app/views/users/view.php
+++ app/views/users/view.php
@@ -5,6 +5,8 @@
 <title>ProfileVault Suite</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="<?= base_url() ?>public/favicon.ico">
+
 
 <style>
 :root{
